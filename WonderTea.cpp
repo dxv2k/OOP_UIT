e0dcacbdbf226265bc8533b2223dee49c8752daa
dxv2k <9569076+dxv2k@users.noreply.github.com>
2019-06-06 23:50:32 +0700

    Chỉ có khung hàm, code cụ thể chưa làm

---
+++ WonderTea.cpp
@@ -0,0 +1,139 @@
+#include <iostream> 
+#define AN_TRUA 600000
+#define DI_LAI 300000
+using namespace std; 
+class NhanVien
+{
+private: 
+	string name; 
+	string address; 
+	string birth; 
+	int phone_number; 
+public: 
+	NhanVien()
+	{}
+	NhanVien(string name, string address,
+			string birth, int phone_number)
+	{
+		this -> name = name; 
+		this -> address = address;
+		this -> birth = birth;
+		this -> phone_number = phone_number; 
+	}
+	virtual void Nhap(); 
+	virtual void Xuat(); 
+	virtual float TinhLuong() = 0;
+};
+class FullTime: public NhanVien 
+{ 
+private:
+	int LuongThoaThuan; 
+public: 
+	FullTime(int LuongThuaThuan)
+	{ this -> LuongThoaThuan = LuongThoaThuan;}
+	float TinhLuong()
+	{ /* return theo công thức trên đề */}
+	void Nhap(); 
+	void Xuat(); 
+};
+class PartTime: public NhanVien 
+{ 
+private:
+	int GioLamViec; 
+public:
+	PartTime(int GioLamViec)
+	{ this -> GioLamViec = GioLamViec;}
+	void Nhap();
+	void Xuat();
+	float TinhLuong()
+	{ /* return theo công thức trên đề */}
+};
+class WonderTea
+{
+private:
+	NhanVien **a; 
+	int SoLuongNV; 
+public: 
+	WonderTea()
+	{}
+	WonderTea(int n)
+	{ a = new NhanVien*[n+1]; }
+	void NhapNV();
+	void XuatNV();
+	void LuongCaoNhat();
+	void LuongDuoiTB(); 
+};
+/*----------------Overload_Operator-----------------*/
+ostream &operator<<(ostream &os, WonderTea &obj)
+{
+	obj.NhapNV(); 
+}
+istream &operator>>(istream &is, WonderTea &obj)
+{
+	obj.XuatNV(); 
+	obj.LuongCaoNhat(); 
+	obj.LuongDuoiTB(); 
+}
+/*-------------------------------------------------*/
+
+int main()
+{ 
+	WonderTea a(30); 
+	cin >> a;
+	cout << a;
+	system("pause"); 
+}
+/*---------------------WonderTea-------------------*/
+void WonderTea::NhapNV()
+{
+	/* Hoàn thiện hàm */
+}
+void WonderTea::XuatNV()
+{
+	/* Hoàn thiện hàm */
+}
+void WonderTea::LuongCaoNhat()
+{
+	/* Hoàn thiện hàm */
+}
+void WonderTea::LuongDuoiTB()
+{
+	/* Hoàn thiện hàm */
+	
+}
+/*-------------------------------------------------*/
+/*---------------------Nhap------------------------*/
+void NhanVien::Nhap()
+{ 
+	/* Hoàn thiện hàm */
+
+}
+void PartTime::Nhap()
+{
+	/* Hoàn thiện hàm */
+
+}
+void FullTime::Nhap()
+{
+	/* Hoàn thiện hàm */
+
+}
+/*-------------------------------------------------*/
+
+
+/*---------------------Xuat------------------------*/
+void NhanVien::Xuat()
+{ 
+
+}
+void PartTime::Xuat()
+{
+	/* Hoàn thiện hàm */
+	
+}
+void FullTime::Xuat()
+{
+	/* Hoàn thiện hàm */
+
+}
+/*-------------------------------------------------*/
